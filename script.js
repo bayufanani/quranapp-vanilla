5297adcfa6249899db69953c5f68cfcdc4355208
yvza <akhmadyuza@gmail.com>
2022-03-25 22:44:36 +0700

    change resolve

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
 function makeRequest(method, url) {
     return new Promise(function (resolve, reject) {
-        let xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.onload = function () {
-            if (this.status >= 200 && this.status < 300) {
-                resolve(JSON.parse(xhr.response));
-            } else {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                });
-            }
-        };
-        xhr.onerror = function () {
-            reject({
-                status: this.status,
-                statusText: xhr.statusText
-            });
-        };
-        xhr.send();
+        resolve({ msh: "hello" });
     });
 }
 
