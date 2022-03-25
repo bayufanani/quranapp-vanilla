1b7205e8a62efd94995d1035cf5a52d73cf0b12d
Bayu Fanani <fanani707@gmail.com>
2022-03-25 23:00:49 +0700

    restore promise

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function makeRequest(method, url) {
     return new Promise(function (resolve, reject) {
-        resolve({ msh: "hello" });
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(JSON.parse(xhr.response));
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
     });
 }
 
